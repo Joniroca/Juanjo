f4588d8a3e4aa681d6c86787c9deec7d92662113
Joan <ranmahito@gmail.com>
2023-05-28 15:53:53 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hola, soy Juanjo.</h1>
+      <img src="https://i.ytimg.com/vi/uCtaRYqfBYk/maxresdefault.jpg" alt="" />
+      <h3>Las cosas que me gustan:</h3>
+      <ul>
+        <li>
+          <b>1ro.</b> Las palomas
+        </li>
+        <li>
+          <b>2do.</b> Los perritos
+        </li>
+        <li>
+          <b>3ro.</b> Jugar GTA-V
+        </li>
+        <li>
+          <b>4to.</b>Pasar tiempo con mi tio Juanito
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
